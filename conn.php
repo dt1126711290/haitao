86e62d2392bb127a37cef5b748642b37fcc383fb
大头 <dt1126711290@hotmail.com>
2020-09-21 14:32:23 +0800

    更新 conn.php
    databse

--- conn.php
+++ conn.php
@@ -1,8 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "haitao";
-
+$username = "db";
+$password = "123456";
+$dbname = "db";
 $conn = new mysqli($servername, $username, $password, $dbname);
 @mysqli_set_charset($conn,utf8);
